effaddf754c47bf906892cd9227d80bbc81898f8
csqly <2543599672@qq.com>
2019-07-04 20:00:09 +0800

    修改

---
+++ application/api/controller/AppHome.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: csq
+ * Date: 2019/7/4
+ * Time: 17:35
+ */
+namespace app\api\controller;
+
+use think\Controller;
+
+class AppHome extends Controller{
+
+    //喜欢
+    public function like(){
+
+    }
+    //推荐
+    public function recommend(){
+
+    }
+    //栏目
+    public function column(){
+
+    }
+
+}
