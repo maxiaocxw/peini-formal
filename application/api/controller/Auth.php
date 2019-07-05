6ba03f83c632a15862cf791eaf8bb5609618f5a7
unknown <986445685@qq.com>
2019-07-05 11:20:31 +0800

    zzl

--- application/api/controller/Auth.php
+++ application/api/controller/Auth.php
@@ -2,8 +2,7 @@
 namespace app\api\controller;
 use think\Controller;
 use think\Db;
-use think\Session;
-
+use app\api\controller\SignatureHelper;
 class Auth extends Controller{
 	public function __construct(){
 		parent::__construct();
@@ -23,8 +22,6 @@ class Auth extends Controller{
             'data'  => $data
         ];
         echo json_encode($arr);die;
-<<<<<<< HEAD
-=======
     }
 
     /**
@@ -59,11 +56,95 @@ class Auth extends Controller{
     public function checkToken(){
         $uid=input('post.uid');
         $token=input('post.token');
-        if(Db::name('user')->where('token='$token.' and uid='.$uid)->find()){
+        if(empty($token)){
+            $this->II('104','缺少token',array());
+        }
+        if(Db::name('user')->where("token='".$token."' and uid=".$uid)->find()){
             return true;
         }else{
             $this->II('103','token错误',array());
         }
->>>>>>> 210dc8cdb6c19ef6bbb4ce99b7e7a63a84a15609
+    }
+
+    //发送验证码接口
+    public function putCode(){
+        $this->checkParam();
+        $phone=input('post.phone');
+        $code=mt_rand(100000,999999);
+        if(!$phone){
+            $this->II('100','参数错误',array());
+        }
+        if($this->sendSms($phone,$code)){
+            cache($phone,$code,30);
+            json_echo(200,'发送成功');
+        }else{
+            json_echo(501,'发送失败,请稍后重试');
+        }
+    }
+
+    //验证码校验
+    public function checkCode($phone,$code){
+        if(!cache($phone)){
+            $this->II('106','验证码已过期',array());
+        }
+        if(cache($phone)==$code){
+            return true;
+        }else{
+            $this->II('105','验证码错误',array());
+        }
+    }
+
+    public function sendSms($phone,$code){
+        $params = array ();
+        //阿里云的AccessKey
+        $accessKeyId = 'LTAIyqG7sDqEkEgA';
+        //阿里云的Access Key Secret
+        $accessKeySecret = 'fzll52W1aDMSCCkFv4e2jCli9qCjCt';
+        //要发送的手机号
+        $params["PhoneNumbers"] = $phone;
+        //签名，第三步申请得到
+        $params["SignName"] = '陪你陪玩';
+        //模板code，第三步申请得到
+        $params["TemplateCode"] = 'SMS_169103077';
+        //模板的参数，注意code这个键需要和模板的占位符一致
+        $params['TemplateParam'] = Array (
+            "code" => $code
+        );
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+        try{
+            // 此处可能会抛出异常，注意catch
+            $content = $helper->request(
+                $accessKeyId,
+                $accessKeySecret,
+                "dysmsapi.aliyuncs.com",
+                array_merge($params, array(
+                    "RegionId" => "cn-hangzhou",
+                    "Action" => "SendSms",
+                    "Version" => "2017-05-25",
+                ))
+            // fixme 选填: 启用https
+            // ,true
+            );
+            $res=array('errCode'=>0,'msg'=>'ok');
+            if($content->Message!='OK'){
+                return false;
+            }
+            return true;
+        }catch(\Exception $e){
+            return false;
+        }
+    }
+
+    //获取用户信息
+    public function getUserInfo($uid){
+        $list=Db::name('user')->field('uid,username,type,status,sex,mobile,birthday,info,headimg,level,addtime,token')->where('uid='.$uid)->find();
+        $list['birthday']=date('Y-m-d',$list['birthday']);
+        $list['addtime']=date('Y-m-d',$list['addtime']);
+        return $list;
     }
 }
