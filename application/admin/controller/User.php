4e4e5b7c3ab33dded9acec14d130d43ee930f676
unknown <986445685@qq.com>
2019-07-15 17:20:15 +0800

    zzl

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller;
 use app\admin\controller\Auth;
+use app\api\controller\Qiniu;
 use think\Db;
 use think\Request;
 use think\Session;
@@ -27,7 +28,31 @@ class User extends Auth{
 	}
 	public function updatedo(){
 		$arr=input('post.');
+		$arr['birthday']=strtotime($arr['birthday']);
+		unset($arr['file']);
 		Db::name('user')->where('uid='.$arr['uid'])->update($arr);
 		$this->success('修改成功','/admin/user/index','1');
 	}
+
+	//七牛图片上传
+    public function qinui_upload(){
+        $qiniu = new Qiniu();
+        $file = $_FILES['file'];
+        if( is_uploaded_file( $file['tmp_name'] ) ){
+            //获取文件路径和文件后缀名
+            $ext = pathinfo( $file['name'], PATHINFO_EXTENSION );
+            //调用方法将图片上传到七牛
+            $qiniu_res = $qiniu->upload( 'image', '.'.$ext, $file['tmp_name'] );
+            //判断结果并返回完整路径
+            if( $qiniu_res ){
+                //将结果转化成数组获取key
+                $qiniu_arr = json_decode( $qiniu_res , true );
+                echo json_encode(['code' => 0,'msg' => '上传成功','icon' => 1,'src'=>$qiniu_arr['key']]);
+            }else{
+                echo json_encode(['code' => 1,'msg' => '上传失败','icon' => 2]);
+            }
+        }else{
+            echo json_encode(['code' => 1,'msg' => '上传错误','icon' => 2]);
+        }
+    }
 }
