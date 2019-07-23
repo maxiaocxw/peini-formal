7130f4def765e2f4b8201e78334290f4137389a1
unknown <986445685@qq.com>
2019-07-23 10:22:42 +0800

    zzl

--- application/api/controller/Gift.php
+++ application/api/controller/Gift.php
@@ -8,16 +8,20 @@ class Gift extends Auth{
 	public function _initialize(){
 		$this->checkParam();
 		$this->checkToken();
-		$this->file=new File();
+		$this->uid=input('post.uid')?input('post.uid'):0;
+		$this->checkParam('uid');
+		//$this->file=new File();
 		$this->url='http://cdn.lanyushiting.com/';
 	}
 	public function index(){
-		if($this->file->has('gift')){
+		/*if($this->file->has('gift')){
 			$this->II('200','获取成功',$this->file->get('gift'));
-		}else{
-			$list=Db::name('gift')->field("gid,name,price,CONCAT('".$this->url."',img)as img")->where('status=1')->order('order asc')->select();
-			$this->file->set('gift',$list,21600);//Valid time 6 hours
-			$this->II('200','获取成功',$list);
-		}
+		}else{*/
+		$list['giftinfo']=Db::name('gift')->field("gid,name,price,CONCAT('".$this->url."',img)as img")->where('status=1')->order('order asc')->select();
+		$list['currency']=Db::name('user')->where('uid='.$this->uid)->value('currency');
+		$list['currency']=$list['currency']?$list['currency']:0;
+		//$this->file->set('gift',$list,21600);//Valid time 6 hours
+		$this->II('200','获取成功',$list);
+		//}
 	}
 }
